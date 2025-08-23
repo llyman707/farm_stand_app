d3b5e7ce2eedad3c0becb8c0f5181f3a303fcd5d
llyman707 <llyman707@gmail.com>
2025-08-23 09:59:39 -0400

    Adding a route to view product details using express

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewURLParser: true,
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('Woof!')
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products });
 })
 
-app.listen(3000, () => {
-    console.log("App is listening on port 3000")
+app.listen(4000, () => {
+    console.log("App is listening on port 4000")
 })
