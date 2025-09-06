4bc10c7bd6a31f1c930c5091953b933a27bf22d1
llyman707 <llyman707@gmail.com>
2025-09-06 17:09:52 -0400

    adding new farm page and associating products with farms

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const methodOverride = require('method-override')
 const AppError = require('./AppError');
 
 const Product = require('./models/product');
+const Farm = require('./models/farm');
+const { emitWarning } = require('process');
+const { read } = require('fs');
+const categories = ['fruit', 'vegetable', 'dairy'];
 
 mongoose.connect('mongodb://localhost:27017/farmStand', { useNewURLParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -22,7 +26,42 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
-const categories = ['fruit', 'vegetable', 'dairy'];
+app.get('/farms', async (req, res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', {farms})
+})
+
+app.get('/farms/:id', async (req, res) => {
+    const farm = await Farm.findById(req.params.id);
+    res.render('farms/show', { farm })
+})
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+app.post('/farms', async (req, res) => {
+    const farm = new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+app.get('/farms/:id/products/new', (req, res) => {
+    const { id } = req.params;
+    res.render('products/new', { categories, id })
+})
+
+app.post('/farms/:id/products',async (req, res) => {
+    const { id } = req.params;
+    const farm = await Farm.findById(id);
+    const { name, price, category } = req.body;
+    const product = new Product({ name, price, category });
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.send(farm)
+})
 
 function wrapAsync(fn) {
     return function (req, res, next) {
